46a204081dae11d85013b0b9b053f8fafd3084d8
yungchih0209 <72060126+yungchih0209@users.noreply.github.com>
2021-02-05 16:20:11 +0800

    Update Self_Study_report.cpp

--- Self_Study_report.cpp
+++ Self_Study_report.cpp
@@ -1,39 +1,226 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-const int maxn = 100 + 10;
-int main() {
-	int loop;
-	cin >> loop;
-	while (loop--) {
-		int n, a[maxn];
-		cin >> n;
-		for (int i = 0; i < n;i++) {
-			cin >> a[i];
-		}
-		bool isque = true, issta = true;
-		for (int i = 0; i < n;i++) {
-			int x;
-			cin >> x;
-			if (x!=a[i]) {
-				isque = false;
+int main()
+{
+	int n;
+	int m;
+	int array[50][50] = { 0 };
+	int x, y;
+	int count;
+	//n為原始陣列邊長
+	//m為起始方向0123左上右下
+	while (cin >> n >> m)
+	{
+		count = 1;
+		for (int i = 1; i <= n; i++)
+			for (int j = 1; j <= n; j++)
+				cin >> array[i][j];
+		y = n / 2 + 1;
+		x = n / 2 + 1;
+		cout << array[y][x];
+		switch (m)
+		{
+			//左上右下
+		case 0:
+			for (int i = 1;; i += 2)
+			{
+				//左x--
+				for (int j = 1; j <= i; j++)
+				{
+					if (count == n * n)
+						break;
+					count++;
+					x--;
+					cout << array[y][x];
+				}
+				if (count == n * n)
+					break;
+				//上y--
+				for (int j = 1; j <= i; j++)
+				{
+					if (count == n * n)
+						break;
+					count++;
+					y--;
+					cout << array[y][x];
+				}
+				if (count == n * n)
+					break;
+				//右x++
+				for (int j = 1; j <= i + 1; j++)
+				{
+					if (count == n * n)
+						break;
+					count++;
+					x++;
+					cout << array[y][x];
+				}
+				if (count == n * n)
+					break;
+				//下y++
+				for (int j = 1; j <= i + 1; j++)
+				{
+					if (count == n * n)
+						break;
+					count++;
+					y++;
+					cout << array[y][x];
+				}
+				if (count == n * n)
+					break;
 			}
-			if (x != a[n-i-1]) {
-				issta = false;
+			break;
+			//上右下左
+		case 1:
+			for (int i = 1;; i += 2)
+			{
+				//上y--
+				for (int j = 1; j <= i; j++)
+				{
+					if (count == n * n)
+						break;
+					count++;
+					y--;
+					cout << array[y][x];
+				}
+				if (count == n * n)
+					break;
+				//右x++
+				for (int j = 1; j <= i; j++)
+				{
+					if (count == n * n)
+						break;
+					count++;
+					x++;
+					cout << array[y][x];
+				}
+				if (count == n * n)
+					break;
+				//下y++
+				for (int j = 1; j <= i + 1; j++)
+				{
+					if (count == n * n)
+						break;
+					count++;
+					y++;
+					cout << array[y][x];
+				}
+				if (count == n * n)
+					break;
+				//左x--
+				for (int j = 1; j <= i + 1; j++)
+				{
+					if (count == n * n)
+						break;
+					count++;
+					x--;
+					cout << array[y][x];
+				}
+				if (count == n * n)
+					break;
 			}
-		}
-		if (issta && isque) {
-			cout << "both" << endl;
-		}
-		else if (issta) {
-			cout << "stack" << endl;
-		}
-		else if (isque) {
-			cout << "queue" << endl;
-		}
-		else {
-			cout << "neither" << endl;
+			break;
+			//右下左上
+		case 2:
+			for (int i = 1;; i += 2)
+			{
+				//右x++
+				for (int j = 1; j <= i; j++)
+				{
+					if (count == n * n)
+						break;
+					count++;
+					x++;
+					cout << array[y][x];
+				}
+				if (count == n * n)
+					break;
+				//下y++
+				for (int j = 1; j <= i; j++)
+				{
+					if (count == n * n)
+						break;
+					count++;
+					y++;
+					cout << array[y][x];
+				}
+				if (count == n * n)
+					break;
+				//左x--
+				for (int j = 1; j <= i + 1; j++)
+				{
+					if (count == n * n)
+						break;
+					count++;
+					x--;
+					cout << array[y][x];
+				}
+				if (count == n * n)
+					break;
+				//上y--
+				for (int j = 1; j <= i + 1; j++)
+				{
+					if (count == n * n)
+						break;
+					count++;
+					y--;
+					cout << array[y][x];
+				}
+				if (count == n * n)
+					break;
+			}
+			break;
+		case 3:
+			//下左上右
+			for (int i = 1;; i += 2)
+			{
+				//下y++
+				for (int j = 1; j <= i; j++)
+				{
+					if (count == n * n)
+						break;
+					count++;
+					y++;
+					cout << array[y][x];
+				}
+				if (count == n * n)
+					break;
+				//左x--
+				for (int j = 1; j <= i; j++)
+				{
+					if (count == n * n)
+						break;
+					count++;
+					x--;
+					cout << array[y][x];
+				}
+				if (count == n * n)
+					break;
+				//上y--
+				for (int j = 1; j <= i + 1; j++)
+				{
+					if (count == n * n)
+						break;
+					count++;
+					y--;
+					cout << array[y][x];
+				}
+				if (count == n * n)
+					break;
+				//右x++
+				for (int j = 1; j <= i + 1; j++)
+				{
+					if (count == n * n)
+						break;
+					count++;
+					x++;
+					cout << array[y][x];
+				}
+				if (count == n * n)
+					break;
+			}
+			break;
 		}
 	}
-
 	return 0;
 }
